5afa17e13084a9ddd21563ac7150b1e4303c5328
Samuel Alves <samuel.estudo778@gmail.com>
2022-04-08 14:04:12 -0300

    feat: create page Projects

---
+++ src/pages/projetos/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header';
+import { ProjetosContainer } from '../../styles/ProjetosStyles';
+
+export default function Projetos() {
+  return (
+    <ProjetosContainer>
+      <Header />
+
+      <main className="container">
+        <p>oi</p>
+      </main>
+    </ProjetosContainer>
+  );
+}
