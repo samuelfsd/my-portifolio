96e364fa940459e6c2f85dfc2d2c29e6d58b8e2d
Samuel Alves <samuel.estudo778@gmail.com>
2022-04-08 14:32:33 -0300

    feat: create component CardProject

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,3 +1,4 @@
+import CardProject from '../../components/CardProject';
 import Header from '../../components/Header';
 import { ProjetosContainer } from '../../styles/ProjetosStyles';
 
@@ -7,7 +8,30 @@ export default function Projetos() {
       <Header />
 
       <main className="container">
-        <p>oi</p>
+        <CardProject
+          title="Projeto 01"
+          type="Web Site"
+          slug="teste"
+          imgUrl="https://images.prismic.io/portfolioaula/be8779ed-c40f-42f2-bd73-e2065676a687_nuprint1.png?auto=compress,format"
+        />
+        <CardProject
+          title="Projeto 01"
+          type="Web Site"
+          slug="teste"
+          imgUrl="https://images.prismic.io/portfolioaula/be8779ed-c40f-42f2-bd73-e2065676a687_nuprint1.png?auto=compress,format"
+        />
+        <CardProject
+          title="Projeto 01"
+          type="Web Site"
+          slug="teste"
+          imgUrl="https://images.prismic.io/portfolioaula/be8779ed-c40f-42f2-bd73-e2065676a687_nuprint1.png?auto=compress,format"
+        />
+        <CardProject
+          title="Projeto 01"
+          type="Web Site"
+          slug="teste"
+          imgUrl="https://images.prismic.io/portfolioaula/be8779ed-c40f-42f2-bd73-e2065676a687_nuprint1.png?auto=compress,format"
+        />
       </main>
     </ProjetosContainer>
   );
