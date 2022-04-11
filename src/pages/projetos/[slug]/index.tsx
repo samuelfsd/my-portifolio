7b8d98be15fb013a088c570a9400f4861e1e9122
Samuel Alves <samuel.estudo778@gmail.com>
2022-04-11 13:25:42 -0300

    feat: create component BannerProject

--- src/pages/projetos/[slug]/index.tsx
+++ src/pages/projetos/[slug]/index.tsx
@@ -1,3 +1,4 @@
+import BannerProject from '../../../components/BannerProject';
 import Header from '../../../components/Header';
 import { ProjectContainer } from '../../../styles/ProjectStyle';
 
@@ -6,7 +7,11 @@ export default function Project() {
     <ProjectContainer>
       <Header />
 
-      <BannerProject />
+      <BannerProject
+        title="Projeto 01"
+        type="WebSite"
+        imgUrl="https://images.prismic.io/portfolioaula/be8779ed-c40f-42f2-bd73-e2065676a687_nuprint1.png?auto=compress,format"
+      />
 
       <main>
         <p>description</p>
